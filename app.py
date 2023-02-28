f15cc3f8c854099356012841644efc49f29cdf42
nick naglieri <nick331102@yahoo.com>
2023-02-27 20:52:18 -0500

    Sure, here's a possible git commit message for the changes made:
    css
    Copy code
    Add functionality to display available products and allow user to select items to add to cart. Update add_to_cart and remove_from_cart methods to take in a Product object and quantity. Modify code to read from products.csv file instead of hard-coded product objects. Add error handling to input prompts. Update checkout method to correctly format total price in order confirmation message. Add instance creation for Cart object. Add save_order method to Cart class to allow saving order history to file. Make other small improvements and fix various bugs.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import csv
+
 class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
@@ -12,19 +14,45 @@ class Cart:
         self.total = 0
         self.coupon = 0
         self.discount = 0
-     
-     # add items to cart and calculate total price   
+
+    # add items to cart and calculate total price   
     def add_to_cart(self, product, quantity):
         self.items.append((product, quantity))
         self.total += product.price * quantity
         
-     # remove items from cart and calculate total price
+    # remove items from cart and calculate total price
     def remove_from_cart(self, product, quantity):
-        if (product, quantity) in self.items:
-            self.items.remove((product, quantity))
-            self.total -= product.price * quantity
+        self.items.remove((product, quantity))
+        self.total -= product.price * quantity
+    
+    # display available products and allow user to choose items to add to cart
+    def choose_items(self):
+        with open('products.csv', 'r') as f:
+            reader = csv.reader(f)
+            next(reader) # skip header row
+            for i, row in enumerate(reader):
+                print(f"{i+1}. {row[0]} - {row[1]} - ${row[2]}")
+                
+            while True:
+                choice = input("Enter the number of the product you would like to add to your cart (or 'done' to finish): ")
+                if choice == 'done':
+                    break
+                try:
+                    choice = int(choice)
+                    if choice < 1 or choice > i+1:
+                        print("Invalid choice. Please try again.")
+                        continue
+                    product = self.get_product(choice)
+                    quantity = input("Enter the quantity: ")
+                    quantity = int(quantity)
+                    self.add_to_cart(product, quantity)
+                    print(f"{quantity} {product.name}(s) added to cart.")
+                except ValueError:
+                    print("Invalid choice. Please try again.")
+                    continue
 
     
+                    
    # Implement the checkout feature: Create a function that allows the user to check out and complete their purchase. This function should calculate the total price of the cart, apply any applicable discounts or coupons, and generate an order confirmation.
    
    # calculate the total price of the cart after applying discounts and coupons
@@ -34,27 +62,62 @@ class Cart:
             total += product.price * quantity
         total -= self.discount
         total -= self.coupon
-        print(f"Thank you for tshopping with us.  Your total is {total}, your confirmation number is 024934782, and your product will ship in 1-3 business days.  Have a great day!")
+        print(f"Thank you for shopping with us. Your total is ${total:.2f}. Your confirmation number is 024934782, and your product will ship in 1-3 business days. Have a great day!")
         
 # Save the order history: Save the order history in a database or a file, so that the user can view their past orders.
     def save_order(self):
         with open('order_history.txt', 'a') as f:
             f.write(f"{self.items}, {self.total}, {self.coupon}, {self.discount}")
         
-# create instances of the Product class
-python_course = Product("Python Programming: An Introduction", "Learn Python from scratch in this beginner's course", 50, 10)
-data_science_course = Product("Data Science for Everyone", "A comprehensive guide to data science", 80, 5)
 
-# create an instance of the Cart class
+# create an empty list to hold products
+products = []
+
+# read products from the CSV file and append to the list
+with open('products.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        products.append(Product(row['product_name'], row['description'], float(row['price']), int(row['quantity'])))
+
+# print the list of products to the user
+print("Welcome to our store! Here are the products available for purchase:")
+for i, product in enumerate(products):
+    print(f"{i+1}. {product.name} - {product.description} - ${product.price}")
+
 cart = Cart()
 
-# add items to the cart
-cart.add_to_cart(python_course, 2)
-cart.add_to_cart(data_science_course, 1)
+# prompt the user to select a product and quantity to add to the cart
+while True:
+    try:
+        product_index = int(input("Enter the number of the product you want to add to your cart (or 0 to exit): "))
+        if product_index == 0:
+            break
+        product = products[product_index-1]
+        quantity = int(input("Enter the quantity: "))
+        cart.add_to_cart(product, quantity)
+        print(f"{quantity} {product.name} added to cart.")
+    except (ValueError, IndexError):
+        print("Invalid input. Please try again.")
 
-# remove an item from the cart
-cart.remove_from_cart(python_course, 1)
+# print the items in the cart
+print("Items in cart:")
+for product, quantity in cart.items:
+    print(f"{product.name} - {product.description} - ${product.price} - Quantity: {quantity}")
+
+# prompt the user to select a product and quantity to remove from the cart
+while True:
+    try:
+        product_index = int(input("Enter the number of the product you want to remove from your cart (or 0 to exit): "))
+        if product_index == 0:
+            break
+        product, quantity = cart.items[product_index-1]
+        cart.remove_from_cart(product, quantity)
+        print(f"{quantity} {product.name} removed from cart.")
+    except (ValueError, IndexError):
+        print("Invalid input. Please try again.")
 
 # checkout and save the order
+
 cart.checkout()
 cart.save_order()
+
